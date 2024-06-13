52e81bfb6258a39db78101b1265ba62ae49ad219
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2024-06-13 09:02:39 -0400

    Add link for GreatFrontEnd

---
+++ components/Foot.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>🎓 Frontend Interview Prep</h2>
+  <p>
+    One of the pioneers in the software interview prep space, Yangshun Tay, is
+    now offering an extremely comprehensive frontend resource full of coding
+    questions, knowledge challenges, system design problems, and more.
+  </p>
+  <p>
+    You can sign up for as low as $10/mo, which is an <em>amazing</em> deal if
+    you consider the impact a new job can have on your compensation.
+  </p>
+  <p>
+    I'd strongly encourage any frontend engineers looking for new jobs to sign
+    up! If you use the following link, I'll get a small referral bonus:
+  </p>
+  <a href="https://www.greatfrontend.com?fpr=nick53" target="_blank"
+    >Check out GreatFrontEnd now &raquo;</a
+  >
+</template>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,11 @@
 import DefaultTheme from "vitepress/theme";
 import Star from "../../../components/Star.vue";
+import Foot from "../../../components/Foot.vue";
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.component("star", Star);
+    app.component("foot", Foot);
   },
 };
